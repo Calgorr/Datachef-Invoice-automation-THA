10dc227705d832edbd1d95e4edc71c5c95160905
calgor <ahomayoni3@gmail.com>
2024-02-09 01:02:40 +0330

    feat: added the lambda function and its validation stuff

---
+++ src/lambda_functions/notify_slack.py
@@ -0,0 +1,17 @@
+import json
+import urllib.request
+from config import SLACK_WEBHOOK_URL
+
+
+def post_message_to_slack(text, webhook_url=SLACK_WEBHOOK_URL):
+    message = {"text": text}
+    request_data = json.dumps(message).encode("utf-8")
+    request = urllib.request.Request(
+        webhook_url, data=request_data, headers={"Content-Type": "application/json"}
+    )
+    try:
+        with urllib.request.urlopen(request) as response:
+            response_body = response.read().decode("utf-8")
+            print(response_body)
+    except Exception as e:
+        print(e)
---
+++ src/lambda_functions/validate_invoice.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+from config import DESTINATION_BUCKET_NAME
+import re
+from notify_slack import post_message_to_slack
+
+s3_client = boto3.client("s3")
+
+
+def lambda_handler(event, context):
+    username, source_bucket, file_key = extract_info_from_event(event)
+    if not file_type_validated(file_key) or not file_name_validated(file_key):
+        slack_message = f"File {file_key} is not a valid invoice file."
+        post_message_to_slack(slack_message)
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Lambda function execution completed."),
+        }
+    try:
+        s3_client.copy_object(
+            {"Bucket": source_bucket, "Key": file_key},
+            DESTINATION_BUCKET_NAME,
+            username + "/" + file_key,
+        )
+        print(f"File {file_key} copied to {DESTINATION_BUCKET_NAME} successfully.")
+    except Exception as e:
+        print(e)
+        slack_message = f"Error processing file {file_key}: {str(e)}"
+        post_message_to_slack(slack_message)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Lambda function execution completed."),
+    }
+
+
+def extract_info_from_event(event):
+    return (
+        event["Records"][0]["userIdentity"]["principalId"],
+        event["Records"][0]["s3"]["bucket"]["name"],
+        event["Records"][0]["s3"]["object"]["key"],
+    )
+
+
+def file_type_validated(file_name: str):
+    return file_name.endswith(".pdf")
+
+
+def file_name_validated(file_name: str):
+    return bool(re.match(r"^\d{6}$", file_name))
