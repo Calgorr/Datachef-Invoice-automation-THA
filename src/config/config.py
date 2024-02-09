a8b5fcf457e62d2c0b2da96f358cd527ba8b99d1
calgor <ahomayoni3@gmail.com>
2024-02-09 11:46:09 +0330

    fix: getting required variables from environment variables

--- src/config/config.py
+++ src/config/config.py
@@ -1,2 +1,11 @@
-DESTINATION_BUCKET_NAME = "destination-bucket-name"
-SLACK_WEBHOOK_URL = "YOUR_SLACK_WEBHOOK_URL"
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DESTINATION_BUCKET_NAME = os.getenv("destination_bucket_name")
+SOURCE_BUCKET_NAME = os.getenv("source_bucket_name")
+SLACK_WEBHOOK_URL = os.getenv("slack_webhook_url")
+ADMIN_PUBLIC_KEY = os.getenv("admin_public_key")
+PUBLIC_SFTP_SERVER_ID = os.getenv("public_sftp_server_id")
